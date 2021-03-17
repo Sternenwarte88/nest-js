ef68baceda07b5cd230d3b7949505dbf5ed7f778
Wusabinga <s.loecher.slc@gmail.com>
2021-03-17 10:00:28 +0100

    🐳chore(main.ts): Base config Express
    Get Main.ts ready with express.

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,19 @@
 import { NestFactory } from '@nestjs/core';
+import { NestExpressApplication } from '@nestjs/platform-express';
+import { join } from 'node:path';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   await app.listen(3000);
 }
+async function bootstrap() {
+  const app = await NestFactory.create<NestExpressApplication>(AppModule);
+
+  app.useStaticAssets(join(__dirname, ' ', 'dev-labs/public'));
+  app.setBaseViewsDir(join(__dirname, ' ', 'dev-labs/views'));
+
+  await app.listen(3000);
+}
+
 bootstrap();
